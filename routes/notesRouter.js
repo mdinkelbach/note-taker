d799565cb4fb80b691a10646fb4950d709a72907
mdinkelbach <mj.dinkelbach@mgail.com>
2022-12-21 11:20:14 -0600

    server

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,51 +1,33 @@
-const express = require('express')
-const router = express.Router()
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-const uuid = require('../helpers/uuid');
+const fb = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
-const app = express();
-
-// GET Route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+fb.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
-// GET Route for retrieving all the notes
-app.get('/api/notes', (req, res) => {
-    console.info(`${req.method} request received for notes`);
-  
-    readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
-  });
+fb.post('/', (req, res) => {
+  const { email, notesType, notes } = req.body;
 
-// POST Route for submitting notes
-app.post('/api/notes', (req, res) => {
-    // Log that a POST request was received
-    console.info(`${req.method} request received to submit notes`);
-  
-    // Destructuring assignment for the items in req.body
-    const { email, notesType, notes } = req.body;
-  
-    // If all the required properties are present
-    if (email && notesType && notes) {
-      // Variable for the object we will save
-      const newNotes = {
-        email,
-        notesType,
-        notes,
-        notes_id: uuid(),
-      };
-  
-      readAndAppend(newNotes, './db/notes.json');
-  
-      const response = {
-        status: 'success',
-        body: newNotes,
-      };
-  
-      res.json(response);
-    } else {
-      res.json('Error in posting notes');
-    }
-  });
+  if (email && notesType && notes) {
+    const newNotes = {
+      email,
+      notesType,
+      notes,
+      notes_id: uuidv4(),
+    };
 
-module.exports = router
+    readAndAppend(newNotes, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNotes,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting notes');
+  }
+});
+
+module.exports = fb;
