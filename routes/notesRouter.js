823a921e001e4963259c78f2af9476d8a02e7771
mdinkelbach <mj.dinkelbach@mgail.com>
2022-12-23 09:25:12 -0600

    existing

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,19 +1,19 @@
-const fb = require('express').Router();
+const noteApp = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const note = require('../db/db.json')
 
-fb.get('/', (req, res) =>
+noteApp.get('/', (req, res) =>
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
-fb.post('/', (req, res) => {
-  const { email, notesType, notes } = req.body;
+noteApp.post('/', (req, res) => {
+  const { title, text} = req.body;
 
-  if (email && notesType && notes) {
+  if (title && text) {
     const newNotes = {
-      email,
-      notesType,
-      notes,
+      title,
+      text,
       notes_id: uuidv4(),
     };
 
@@ -30,4 +30,21 @@ fb.post('/', (req, res) => {
   }
 });
 
-module.exports = fb;
+noteApp.get('/:notes_id', (req, res) => {
+  if (req.params.notes_id) {
+    console.info(`${req.method} request received to get a single note`);
+    const noteId = req.params.notes_id;
+    for (let i = 0; i < note.length; i++) {
+      const currentNote = note[i];
+      if (currentNote.notes_id === noteId) {
+        res.json(currentNote);
+        return;
+      }
+    }
+    res.status(404).send('Note not found');
+  } else {
+    res.status(400).send('Note ID not provided');
+  }
+});
+
+module.exports = noteApp;
